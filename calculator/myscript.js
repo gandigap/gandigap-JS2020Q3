559c6fed32921fa89b0fd51b9f7cdbbed38b97d9
Ihar Berasneu <igor.simatic@gmail.com>
2020-09-24 14:01:32 +0300

    feat:add some changes

--- calculator/myscript.js
+++ calculator/myscript.js
@@ -49,6 +49,9 @@ class Calculator {
             case '÷':
                 computation = prev / current;
                 break
+            case '^':
+                computation = prev / current;
+                break;
             default:
                 return;
         }
